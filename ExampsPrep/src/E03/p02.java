034aef65382f069e597097e093a2a6c4513fe767
Idaki Milev <idaki@me.com>
2023-04-27 13:34:37 +0200

    update Fundamentals  Folder Org

--- ExampsPrep/src/E03/p02.java
+++
@@ -1,9 +0,0 @@
-package E03;
-
-import java.util.Scanner;
-
-public class p02 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    }
-}
--- ExampsPrep/src/E03/p03.java
+++
@@ -1,9 +0,0 @@
-package E03;
-
-import java.util.Scanner;
-
-public class p03 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    }
-}
